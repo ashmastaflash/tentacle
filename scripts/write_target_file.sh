12c1b4c16a47045e055ba1a32835c5db5dd182af
Ash Wilson <ash.d.wilson@gmail.com>
2015-10-29 12:52:23 -0700

    Accept target DNS name from ENV

--- scripts/write_target_file.sh
+++ scripts/write_target_file.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#export TARGET=`netstat -r | grep default | awk '{print $2}'`
-export TARGET=`ifconfig eth0  | grep 'inet addr' | awk '{print $2}' | awk -F: '{print $2}'`
+if [ -z {$TARGET_DNS+x} ]; then export TARGET=`ifconfig eth0  | grep 'inet addr' | awk '{print $2}' | awk -F: '{print $2}'`; else export TARGET=`host -4 $TARGET_DNS | awk '{print $4}' | head -1`; fi
+#export TARGET=`ifconfig eth0  | grep 'inet addr' | awk '{print $2}' | awk -F: '{print $2}'`
 printf "%s" "$TARGET" > /root/target
