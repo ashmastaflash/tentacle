4aba1520696a4626c1e3138dde970892627f3d15
Ash Wilson <ash.d.wilson@gmail.com>
2015-10-28 18:44:53 -0700

    Punch self in face

--- scripts/write_target_file.sh
+++ scripts/write_target_file.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-export TARGET=`netstat -r | grep default | awk '{print $2}'`
+#export TARGET=`netstat -r | grep default | awk '{print $2}'`
+export TARGET=`ifconfig eth0  | grep 'inet addr' | awk '{print $2}' | awk -F: '{print $2}'`
 printf "%s" "$TARGET" > /root/target
