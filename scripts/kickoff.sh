1548d1bc034ca6f9fb23d37e00a5a59940fec7d7
Ash Wilson <ash.d.wilson@gmail.com>
2015-10-28 18:03:28 -0700

    Tentacle was gagging the target

--- scripts/kickoff.sh
+++ scripts/kickoff.sh
@@ -2,4 +2,4 @@
 /root/register_halo.sh 
 /etc/init.d/cphalod start
 /root/write_target_file.sh
-httperf --server=/root/target --port=80 --num-conn=50000 --rate=100
+httperf --server=/root/target --port=80 --num-conn=50000 --rate=5
