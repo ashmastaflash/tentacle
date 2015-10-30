f3ff82219a73d2c424fcd79bb9aa579cf3bda307
Ash Wilson <ash.d.wilson@gmail.com>
2015-10-30 12:30:30 -0700

    Change httperf kickoff settings

--- scripts/kickoff.sh
+++ scripts/kickoff.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/root/register_halo.sh 
+/root/register_halo.sh
 /etc/init.d/cphalod start
-/root/write_target_file.sh
-httperf --server=/root/target --port=80 --num-conn=50000 --rate=5
+#/root/write_target_file.sh
+httperf --server=$TARGET_DNS --port=80 --num-conn=50000 --rate=5
